a29675f4044f8d0e543437bf9c5e7426f2a8ee35
TestSoulja <serzh.ekcker@yandex.ru>
2024-06-20 10:35:58 +0300

    renamed:    VideoConverter/Enter/Filler.md -> VideoCompression/Enter/Filler.md 	renamed:    VideoConverter/Exit/Filler.md -> VideoCompression/Exit/Filler.md 	modified:   VideoCompression/main.py 	deleted:    VideoConverter/main.py

--- VideoCompression/main.py
+++ VideoCompression/main.py
@@ -4,5 +4,12 @@ import os
 s = os.path.abspath(__file__)
 c = s.replace(os.path.basename(os.path.abspath(__file__)), '')
 
-clip = moviepy.VideoFileClip(c+"3.avi")
-clip.write_videofile(c+"3.mp4")
+counter = 0
+
+directory = c+ 'Enter'
+for filename in os.listdir(directory):
+    f = os.path.join(directory, filename)
+    if os.path.isfile(f):
+        clip = moviepy.VideoFileClip(c+"Enter/"+ filename)
+        clip.write_videofile(c+"Exit/"+ str(counter)+".mp4")
+        counter +=1
